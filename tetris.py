d7ed9e80eee80d514394d343ce66962815daa72c
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-03-07 15:41:27 -0500

    Add tetromino data

--- tetris.py
+++ tetris.py
@@ -1,6 +1,53 @@
 import pygame
 
 
+TETROMINO_I = [
+    0, 0, 0, 0,
+    1, 1, 1, 1,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    ]
+TETROMINO_J = [
+    2, 0, 0,
+    2, 2, 2,
+    0, 0, 0,
+    ]
+TETROMINO_L = [
+    0, 0, 3,
+    3, 3, 3,
+    0, 0, 0,
+    ]
+TETROMINO_O = [
+    4, 4,
+    4, 4,
+    ]
+TETROMINO_S = [
+    0, 5, 5,
+    5, 5, 0,
+    0, 0, 0,
+    ]
+TETROMINO_T = [
+    0, 6, 0,
+    6, 6, 6,
+    0, 0, 0,
+    ]
+TETROMINO_Z = [
+    7, 7, 0,
+    0, 7, 7,
+    0, 0, 0,
+    ]
+
+TETROMINOS = [
+    dict(data=TETROMINO_I, size=4),
+    dict(data=TETROMINO_J, size=3),
+    dict(data=TETROMINO_L, size=3),
+    dict(data=TETROMINO_O, size=2),
+    dict(data=TETROMINO_S, size=3),
+    dict(data=TETROMINO_T, size=3),
+    dict(data=TETROMINO_Z, size=3),
+]
+
+
 def main():
     pass
 
