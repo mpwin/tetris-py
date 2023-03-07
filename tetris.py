cb762bd81948b321279257c640936dbed7558241
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-03-07 15:40:06 -0500

    Initial commit

---
+++ tetris.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+def main():
+    pass
+
+
+class Game:
+
+    def __init__(self):
+        pass
+
+
+class Tetromino:
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()
