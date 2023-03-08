8cca8d4d4d86e95c3d9eeec5285508cd771f8e52
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-03-08 15:32:29 -0500

    Init pygame and create window

--- tetris.py
+++ tetris.py
@@ -49,7 +49,16 @@ TETROMINOS = [
 
 
 def main():
-    pass
+    pygame.init()
+    pygame.display.set_mode([640, 480])
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
 
 
 class Game:
