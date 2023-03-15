742bf92833a9731418b844ae17205b498d5d8eb5
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-03-15 16:01:34 -0400

    ESCAPE key now closes the game

--- tetris.py
+++ tetris.py
@@ -71,6 +71,8 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
                 if event.key == pygame.K_UP:
                     input.add('up')
                 if event.key == pygame.K_LEFT:
